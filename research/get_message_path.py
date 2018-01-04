50dc9299c563e50d9faea072eb40d445eff78139
meidan <meidan.kaneti@superfly.com>
2018-01-04 18:18:57 +0200

    Added research file

---
+++ research/get_message_path.py
@@ -0,0 +1,10 @@
+from main import get_message_full_file_path
+
+
+print(get_message_full_file_path(735693674))
+
+
+print(get_message_full_file_path(232))
+print(get_message_full_file_path(2332))
+print(get_message_full_file_path(23332))
+print(get_message_full_file_path(233332))
